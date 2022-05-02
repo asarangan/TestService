09b37498f433ec236b4f655240ecb49ffcb6c7e4
asarangan <asarangan@gmail.com>
2022-05-02 17:04:34 -0400

    tes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,68 +15,66 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     var msg = "Testing : "
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.d(msg, "The onCreate() event")
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
 
-        class BindingActivity : Activity() {
-            private lateinit var mService: LocalService
-            private var mBound: Boolean = false
 
-            /** Defines callbacks for service binding, passed to bindService()  */
-            private val connection = object : ServiceConnection {
 
-                override fun onServiceConnected(className: ComponentName, service: IBinder) {
-                    // We've bound to LocalService, cast the IBinder and get LocalService instance
-                    val binder = service as LocalService.LocalBinder
-                    mService = binder.getService()
-                    mBound = true
-                }
 
-                override fun onServiceDisconnected(arg0: ComponentName) {
-                    mBound = false
-                }
+    class BindingActivity : Activity() {
+        private lateinit var mService: LocalService
+        private var mBound: Boolean = false
+
+        /** Defines callbacks for service binding, passed to bindService()  */
+        private val connection = object : ServiceConnection {
+
+            override fun onServiceConnected(className: ComponentName, service: IBinder) {
+                // We've bound to LocalService, cast the IBinder and get LocalService instance
+                val binder = service as LocalService.LocalBinder
+                mService = binder.getService()
+                mBound = true
             }
 
-            override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-                setContentView(R.layout.main)
-            }
-
-            override fun onStart() {
-                super.onStart()
-                // Bind to LocalService
-                Intent(this, LocalService::class.java).also { intent ->
-                    bindService(intent, connection, Context.BIND_AUTO_CREATE)
-                }
-            }
-
-            override fun onStop() {
-                super.onStop()
-                unbindService(connection)
+            override fun onServiceDisconnected(arg0: ComponentName) {
                 mBound = false
-            }
-
-            /** Called when a button is clicked (the button in the layout file attaches to
-             * this method with the android:onClick attribute)  */
-            fun onButtonClick(v: View) {
-                if (mBound) {
-                    // Call a method from the LocalService.
-                    // However, if this call were something that might hang, then this request should
-                    // occur in a separate thread to avoid slowing down the activity performance.
-                    val num: Int = mService.randomNumber
-                    Toast.makeText(this, "number: $num", Toast.LENGTH_SHORT).show()
-                }
             }
         }
 
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+        }
 
+        override fun onStart() {
+            super.onStart()
+            // Bind to LocalService
+            Intent(this, LocalService::class.java).also { intent ->
+                bindService(intent, connection, Context.BIND_AUTO_CREATE)
+            }
+        }
 
+        override fun onStop() {
+            super.onStop()
+            unbindService(connection)
+            mBound = false
+        }
 
-
-
+        /** Called when a button is clicked (the button in the layout file attaches to
+         * this method with the android:onClick attribute)  */
+        fun onButtonClick(v: View) {
+            if (mBound) {
+                // Call a method from the LocalService.
+                // However, if this call were something that might hang, then this request should
+                // occur in a separate thread to avoid slowing down the activity performance.
+                val num: Int = mService.randomNumber
+                Toast.makeText(this, "number: $num", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
+
 
 
 }
